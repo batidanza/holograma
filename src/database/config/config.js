66b86dac879010f14a17877da940834e6a29497a
User <taquinibautista@gmail.com>
2023-09-15 00:58:18 -0300

    holograma saluda al mundo

---
+++ src/database/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "",
+    "password": "",
+    "database": "",
+    "host": "mysql-seagull.alwaysdata.net",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "mysql-seagull.alwaysdata.net",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "mysql-seagull.alwaysdata.net",
+    "dialect": "mysql"
+  }
+}
